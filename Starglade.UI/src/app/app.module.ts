890f16bce28fc1e2a2d851c110820f7410bc3217
Saokat  Ali <saokatali@outlook.com>
2019-08-15 16:25:57 +0530

    Added Layout components

--- Starglade.UI/src/app/app.module.ts
+++ Starglade.UI/src/app/app.module.ts
@@ -2,11 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from './components/app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { BodyComponent } from './components/body/body.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    BodyComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
